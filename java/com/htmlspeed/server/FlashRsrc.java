8c03f1405fbf959da8c800b44a2c9ae1925c315e
Eldad Zamler <eldad314@gmail.com>
2016-02-02 17:49:09 +0200

    Create FlashRsrc.java

---
+++ java/com/htmlspeed/server/FlashRsrc.java
@@ -0,0 +1,20 @@
+/**
+ *  Copyright 2011 Galiel 3.14 Ltd. All rights reserved.
+ *  Use is subject to license terms.
+ *
+ * Created on 17 December 2012
+ */
+package com.htmlspeed.server;
+
+/**
+ * FlashRsrc.
+ *
+ * A swf file (flash presentation).
+ * Used for caching flash files.
+ *
+ * @author  Eldad Zamler
+ * @version $Revision: 1.2 $$Date: 2012/12/17 06:33:17 $
+ */
+public class FlashRsrc extends Rsrc
+{
+}
