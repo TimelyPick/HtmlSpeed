bd0b9f14fa84e5bad5c2ca3a199761d4e5cf54c9
Eldad Zamler <eldad314@gmail.com>
2016-02-02 18:05:39 +0200

    Create RsrcIfc.java

---
+++ java/com/htmlspeed/server/RsrcIfc.java
@@ -0,0 +1,19 @@
+/**
+ *  Copyright 2011 Galiel 3.14 Ltd. All rights reserved.
+ *  Use is subject to license terms.
+ *
+ * Created on 8 March 2012
+ */
+package com.htmlspeed.server;
+
+/**
+ * RsrcIfc
+ *
+ * Implemented by Rsrc and by TempRsrc.
+ *
+ * @author  Eldad Zamler
+ * @version $Revision: 1.1 $$Date: 2012/06/08 10:26:05 $
+ */
+public interface RsrcIfc
+{
+}
